77e7450c6c2e835c2527b260e409ff45ba53b3d1
Kev <11982759+buck-silver@users.noreply.github.com>
2026-01-24 09:45:08 -0700

    added nuke option

---
+++ sql/migrations/000_delete_all.sql
@@ -0,0 +1,7 @@
+-- DANGER: This will delete all data and objects in these schemas!
+drop schema if exists fn cascade;
+drop schema if exists ref cascade;
+drop schema if exists sims1 cascade;
+drop schema if exists sims2 cascade;
+drop schema if exists sims3 cascade;
+drop schema if exists sims4 cascade;
